bd3067c810a4ee1170cbc6a814c90edd9207f919
biacs <biacs@DESKTOP-2KI0JOE>
2018-04-04 23:28:00 -0300

    Creando formulário com thymeleaf

--- compras/src/main/java/br/com/compras/controller/ItemsController.java
+++ compras/src/main/java/br/com/compras/controller/ItemsController.java
@@ -3,20 +3,46 @@ package br.com.compras.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.compras.model.Item;
 import br.com.compras.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("items")
+@Slf4j
 public class ItemsController {
 	
 	@Autowired
 	ItemRepository itemRepository;
 	
-	@RequestMapping("")
+	@GetMapping("")
 	public String getListaItens(Model model) {
-		model.addAttribute("items",itemRepository.findByDescricao("ovo"));
+		model.addAttribute("items",itemRepository.findAll());
+		
+		log.debug("Items recuperados da base!");
+		
+		return "items/show";
+	}
+	
+	@GetMapping("/item")
+	public String salvarItem(Model model) {
+		
+		model.addAttribute("item",new Item());
+		
+		return "items/create";
+	}
+	
+	@PostMapping("/item")
+	public String salvarItem(@ModelAttribute Item item) {
+		
+		itemRepository.save(item);
+		
+		log.debug("Item salvo!");
 		
 		return "items/show";
 	}
--- compras/src/main/java/br/com/compras/repository/ItemRepositoryImpl.java
+++
@@ -1,5 +0,0 @@
-package br.com.compras.repository;
-
-public class ItemRepositoryImpl {
-
-}
